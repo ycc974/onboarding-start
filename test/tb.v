f7ebf0cd3e1e86b9f220f2433b86c67d435c5358
ycchoo <yeanchen2176@gmail.com>
2025-12-31 12:23:20 -0500

    Step 2: update top module, PWM wiring, and project metadata

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+  tt_um_uwasic_onboarding_ycchoo (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
